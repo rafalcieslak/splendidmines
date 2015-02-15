f9175864b4a548f2decdbff61551a00cdfd4e384
Rafał Cieślak <rafalcieslak256@gmail.com>
2015-02-15 20:12:40 +0100

    Sealed the API implementation with 'unimplemented' exceptions

--- frontend/api.cpp
+++ frontend/api.cpp
@@ -5,44 +5,53 @@ using namespace MinesPerfect;
 
 extern MainWindow* pMainWindow;
 
+class UnimplementedAPIException{
+public:
+	UnimplementedAPIException(){}
+};
+
 API::Bitmap* MinesPerfect::API::CreateBitmap (const string& kind, const string& spec){
+	throw UnimplementedAPIException();
 	return nullptr;
 }
 API::Bitmap* MinesPerfect::API::CreateBitmap (const API::Bitmap* from, const Rect& rect){
+	throw UnimplementedAPIException();
 	return nullptr;
 }
 
 API::Timer* MinesPerfect::API::CreateTimer(){
+	throw UnimplementedAPIException();
 	return nullptr;
 }
 
 API::Sound* MinesPerfect::API::CreateSound (const string& name){
+	throw UnimplementedAPIException();
 	return nullptr;
 }
 
 void MinesPerfect::API::Init(){
-
+	throw UnimplementedAPIException();
 }
 
 void MinesPerfect::API::WinSetSize    (const Point& sz){
-
+	throw UnimplementedAPIException();
 }
 void MinesPerfect::API::WinDrawBitmap (const API::Bitmap* bmp, const Point& p){
-
+	throw UnimplementedAPIException();
 }
 void MinesPerfect::API::WinDrawBevel  (const Rect& rect, int thickness, bool raised){
-
+	throw UnimplementedAPIException();
 }
 
 void MinesPerfect::API::DlgNewRecord (Options* options, int num_msecs, bool certified_board){
-
+	throw UnimplementedAPIException();
 }
 void MinesPerfect::API::FindFiles (vector<string>& files, const string& pattern){
-
+	throw UnimplementedAPIException();
 }
 bool MinesPerfect::API::FileExist (const string& fname){
-
+	throw UnimplementedAPIException();
 }
 void MinesPerfect::API::ShowMessageDlg (const string& text, const string& title){
-
+	throw UnimplementedAPIException();
 }
